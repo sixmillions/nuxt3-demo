ad6bb2ce5736632419dc9ca6c41509accc9a9288
sixmillions <liubw95@163.com>
2023-05-14 13:12:42 +0000

    daisyui主题

--- pages/demo/daisyui.vue
+++ pages/demo/daisyui.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+let theme = ref('light')
+const AllTheme: string[] = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"]
+const setColorTheme = (newTheme: string): void => {
+  console.log('修改主题', newTheme)
+  theme.value = newTheme
+  console.log(111, theme.value);
+}
+</script>
+
 <template>
-  <div>
-    DaisyUI安装成功
-    <button class="btn btn-primary">Button</button>
+  <div :data-theme="theme" class="m-10">
+    <div>
+      DaisyUI安装成功
+      <button class="btn btn-primary">Button</button>
+    </div>
+    <div>
+      主题测试修改测试
+      <ul>
+        <li v-for="item in AllTheme" :key="item" @click="setColorTheme(item)">{{ item }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
