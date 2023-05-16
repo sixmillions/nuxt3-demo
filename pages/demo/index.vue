ccdd85a3c05070b9a7c15d7d0b1afdf0f3aa249b
sixmillions <liubw95@163.com>
2023-05-16 14:34:25 +0800

    fix:lute报错

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,7 +1,9 @@
 <template>
-<ul>
-  <li><nuxt-link to="/demo/daisyui">DaisyUI Demo</nuxt-link></li>
-  <li><nuxt-link to="/demo/vditor">Vditor Demo</nuxt-link></li>
-  <li><nuxt-link to="/demo/v-md">v-md-editor Demo</nuxt-link></li>
-</ul>
+  <div class="flex justify-center justify-items-center mt-4">
+    <ul class="font-bold underline text-blue-500 space-y-4 visited:text-red-500">
+      <li><nuxt-link to="/demo/daisyui">DaisyUI Demo</nuxt-link></li>
+      <li><nuxt-link to="/demo/vditor">Vditor Demo</nuxt-link></li>
+      <li><nuxt-link to="/demo/v-md">v-md-editor Demo</nuxt-link></li>
+    </ul>
+  </div>
 </template>
--- pages/demo/vditor.vue
+++ pages/demo/vditor.vue
@@ -3,21 +3,13 @@
 </template>
 
 <script setup lang="ts">
-import Vditor from 'vditor';
-import 'vditor/dist/index.css';
-// 解决Lute找不到问题
-useHead({
-  script: [
-    {
-      src: "https://unpkg.com/vditor@3.9.2/dist/js/lute/lute.min.js",
-      id: "vditorLuteScript",
-    },
-  ],
-});
+import Vditor from 'vditor'
+import 'vditor/dist/index.css'
+
 
 const vditor = ref<Vditor | null>(null);
 
-onMounted(() => {
+const initVditor = () => {
   vditor.value = new Vditor('vditor', {
     height: 720,
     toolbarConfig: {
@@ -28,10 +20,26 @@ onMounted(() => {
     // },
     after: () => {
       // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value!.setValue('# Hello World');
+      vditor.value!.setValue('# Hello World')
     },
-  });
-});
+  })
+}
+
+onMounted(() => {
+  useHead({
+    script: [
+      {
+        src: "https://unpkg.com/vditor@3.9.2/dist/js/lute/lute.min.js",
+        id: "vditorLuteScript",
+        onload: () => {
+          // 解决Lute找不到问题, 加载lute后再执行Vditor创建
+          // 测试发现这种方式部署后没问题，开发环境下还需要手动刷新
+          initVditor()
+        }
+      },
+    ],
+  })
+})
 </script>
 
 <style></style>
