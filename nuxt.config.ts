7a1c2aa9b37507108b88c84cd6cc8c1c8f38f020
sixmillions <liubw95@163.com>
2023-05-16 15:20:11 +0800

    配置读取

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    // 私有配置只在服务器端可用，会被.env中的NUXT_ENV_VAR1覆盖
+    envVar1: 'xxx',
+    // 公共配置也可以在客户端使用，会被.env中的NUXT_PUBLIC_PUBLIC_ENV_VAR1覆盖
+    public: {
+      publicEnvVar1: 'sss'
+    }
+  }
 })
---
+++ pages/demo/env-var.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>环境变量使用 https://nuxt.com/docs/getting-started/configuration</div>
+  <div>替换nuxt.config.ts中的私有配置，需要用“NUXT_”开头，然后变量名驼峰转大写，用下划线分。例如 apiToken -- NUXT_API_TOKEN</div>
+  <div>替换nuxt.config.ts中的公共配置，需要用“NUXT_PUBLIC”开头，然后变量名驼峰转大写，用下划线分隔。例如 pubicKey -- NUXT_PUBLIC_PUBLIC_KEY</div>
+</template>
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+console.log(runtimeConfig.envVar1, runtimeConfig.publicEnvVar1)
+</script>
